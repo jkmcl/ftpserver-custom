59cfe8b1f1e10e2e7b3a4f64ac2aa2845055eb9f
Michael Lee <jkmcl@users.noreply.github.com>
2024-10-25 01:31:31 +0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+A hack to customize the directory listing format of org.apache.ftpserver:ftpserver-core:1.1.4.
+
+1. Compile this project
+
+2. Create directory `custom/lib` under directory where ftpserver was installed
+
+3. Copy JAR created in step 1 to directory created in step 2
+
+4. Overwrite file `bin/ftpd.bat` with one from this project
---
+++ src/main/java/org/apache/ftpserver/command/impl/listing/LISTFileFormater.java
@@ -0,0 +1,63 @@
+package org.apache.ftpserver.command.impl.listing;
+
+import java.util.Arrays;
+
+import org.apache.ftpserver.ftplet.FtpFile;
+import org.apache.ftpserver.util.DateUtils;
+
+public class LISTFileFormater implements FileFormater {
+
+	private static final char DELIM = ' ';
+
+	private static final char[] NEWLINE = { '\n' }; // LF, instead of CR LF
+
+	public String format(FtpFile file) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(getPermission(file));
+		sb.append(DELIM); // 1 space in front of link count, instead of 3
+		sb.append(String.valueOf(file.getLinkCount()));
+		sb.append(DELIM);
+		sb.append(file.getOwnerName());
+		sb.append(DELIM);
+		sb.append(file.getGroupName());
+		sb.append(DELIM);
+		sb.append(getLength(file)); // left-pad to 10 digits, instead of 12
+		sb.append(DELIM);
+		sb.append(getLastModified(file));
+		sb.append(DELIM); // 2 spaces in front of name, instead of 1
+		sb.append(DELIM);
+		sb.append(file.getName());
+		sb.append(NEWLINE);
+
+		return sb.toString();
+	}
+
+	private String getLength(FtpFile file) {
+		String initStr = "          ";
+		long sz = 0;
+		if (file.isFile()) {
+			sz = file.getSize();
+		}
+		String szStr = String.valueOf(sz);
+		if (szStr.length() > initStr.length()) {
+			return szStr;
+		}
+		return initStr.substring(0, initStr.length() - szStr.length()) + szStr;
+	}
+
+	private String getLastModified(FtpFile file) {
+		return DateUtils.getUnixDate(file.getLastModified());
+	}
+
+	private char[] getPermission(FtpFile file) {
+		char[] permission = new char[10];
+		Arrays.fill(permission, '-');
+
+		permission[0] = file.isDirectory() ? 'd' : '-';
+		permission[1] = file.isReadable() ? 'r' : '-';
+		permission[2] = file.isWritable() ? 'w' : '-';
+		permission[3] = file.isDirectory() ? 'x' : '-';
+		return permission;
+	}
+
+}
